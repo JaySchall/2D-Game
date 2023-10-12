76591c03b8dcca43cd6351db6a1fb3fcb5481b83
Jonathan Schall <62366496+JaySchall@users.noreply.github.com>
2023-10-11 21:07:58 -0400

    win conditions

---
+++ Game Design 2D/Assets/Goal.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
+public class Goal : MonoBehaviour
+{
+    private Light2D light2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light2D = GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Power powered = GetComponent<Power>();
+        if (powered.powered)
+        {
+            Win();
+        }
+    }
+
+    void Win()
+    {
+        SceneManager.LoadScene("Level Completed");
+    }
+}
