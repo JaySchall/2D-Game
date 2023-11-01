6ef058196340d082826dce894b98854bdbb01aa0
klumzy33 <92058482+klumzy33@users.noreply.github.com>
2023-11-01 14:52:27 -0400

    LevelSelector set to correct levels

--- Game Design 2D/Assets/LevelSelector.cs
+++ Game Design 2D/Assets/LevelSelector.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class LevelSelector : MonoBehaviour
 {
+    public int level;
+    public Text levelText;
     // Start is called before the first frame update
     // coco Code
     void Start()
     {
-        
+       // levelText.text = level.ToString();
     }
 
     public void OpenScene()
     {
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene("Level-" + level.ToString());
     }
 }
